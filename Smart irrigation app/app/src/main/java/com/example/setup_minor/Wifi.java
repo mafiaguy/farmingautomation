fe23028f9c4ab2816b21c97edbf86eaa8957f65c
Nitin Upadhyay <31355963+NU28@users.noreply.github.com>
2019-10-29 01:17:46 +0530

    Add files via upload

---
+++ Smart irrigation app/app/src/main/java/com/example/setup_minor/Wifi.java
@@ -0,0 +1,13 @@
+package com.example.setup_minor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Wifi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wifi_activity);
+    }
+}
